8c3c174a69f26eb61f257b6248dcac6192fe72af
AlvaroPdev <ampcastro1305@gmail.com>
2025-10-25 01:07:31 -0300

    feat: animations and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,44 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         'inter': ['Inter', 'sans-serif']
-      }
+      },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
+};
+
+// Plugin para agregar colores de Tailwind como variables CSS
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
