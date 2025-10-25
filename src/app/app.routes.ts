8320a8b9127144149fd1e0f73cf169c0d5d58724
AlvaroPdev <ampcastro1305@gmail.com>
2025-10-24 21:27:11 -0300

    feat: new folio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
-
+import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    component: HomeComponent,
   }
 ];
